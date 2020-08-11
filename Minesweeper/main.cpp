3b31c37d774b395eb35fce4797f309aefb9c5729
hsetlik <haydensetlik@gmail.com>
2020-08-10 22:08:11 -0600

    Initial Commit

---
+++ Minesweeper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Minesweeper
+//
+//  Created by Hayden on 8/10/20.
+//  Copyright © 2020 Hayden Setlik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
